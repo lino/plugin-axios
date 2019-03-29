7a3c839f982bb3475fef59daba15f3551985bd62
Lino Helms <lino@betaeg.co>
2019-03-29 13:17:17 +0100

    added docs and typescript config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    title: 'Vuex ORM Plugin Axios',
+    description: 'The Vuex ORM plugin to enable data exchange with HTTP based APIs.',
+
+    base: '/plugin-axios/',
+
+    themeConfig: {
+        repo: 'vuex-orm/plugin-axios',
+        docsDir: 'docs',
+
+        // nav: [
+        //     { text: 'Guide', link: '/guide/prologue/what-is-vuex-orm' },
+        //     { text: 'API Reference', link: '/api/database' },
+        //     { text: 'Release Notes', link: 'https://github.com/vuex-orm/vuex-orm/releases' }
+        // ],
+
+        // sidebar: {
+        //     '/guide/': sidebars.guide,
+        //     '/api/': sidebars.api,
+        //     '/': sidebars.guide
+        // }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# vuex-orm-plugin-axios
+
+plugin-axios is a plugin for [Vuex-ORM](https://github.com/vuex-orm/vuex-orm) allowing object fetching and saving to a HTTP-based API.
